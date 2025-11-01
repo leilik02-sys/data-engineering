9a080f99b70efee9257a11cabd87f20a2c4bb62c
leilik02-sys <leilik02@gmail.com>
2025-11-01 23:51:49 +0700

    Delete data directory

--- data/validate.py
+++
@@ -1,6 +0,0 @@
-def validate_dataframe(df):
-    if df.empty:
-        raise ValueError("Датасет пуст!")
-
-    if "population" in df.columns and df["population"].isna().all():
-        raise ValueError("Колонка population состоит только из NaN")
